693313a5aee66a9943ea3d91f75b1bb2886ef64f
Hans Archer Montezon Dalubatan <dalubatanhans@gmail.com>
2024-12-20 20:33:40 +0800

    form

--- src/components/ResumeForm.tsx
+++ src/components/ResumeForm.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 
 const ResumeForm: React.FC = () => {
     const [formData, setFormData] = useState({
-
         name: '',
         email: '',
         phone: '',
@@ -10,6 +9,18 @@ const ResumeForm: React.FC = () => {
         education: '',
         experience: '',
     });
-}
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    return (
+        <form className="space-y-4">
+        <input type="text" 
+        name="name" 
+        placeholder="name" 
+        value={FormData.name}/>
+        </form>
+    )
 
 export default ResumeForm;
