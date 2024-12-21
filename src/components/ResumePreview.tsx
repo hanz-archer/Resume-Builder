e9aacd77b31f12243e6a19b1dfc12b23453547ef
Hans Archer Montezon Dalubatan <dalubatanhans@gmail.com>
2024-12-21 09:48:53 +0800

    .tsx preview

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 
 interface ResumePreviewProps {
-    formData{
+    formData: {
         name: String;
         email: String;
         phone: String;
         skills: String;
         education: String;
         experience: String;
-    }
+    };
 }
+
+    const ResumePreview: React.FC<ResumePreviewProps> = ({ formData }) => {
+        return(
+            <div className="resume-preview">
+                <h1>{formData.name}</h1>
+                <h1>{formData.email}</h1>
+                <h1>{formData.phone}</h1>
+                <h2>Skills</h2>
+                <p>{formData.skills}</p>
+                <h1>{formData.education}</h1>
+                <h2>Experience</h2>
+                <p>{formData.experience}</p>
+            </div>
+        );
+    };
 
 export default ResumePreview;
